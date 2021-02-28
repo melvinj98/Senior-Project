60e57815bea13fdfc427405b532513879c7e4cc2
melvinj98 <36276556+melvinj98@users.noreply.github.com>
2021-02-28 16:44:43 -0500

    Implemented emotion recognition

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
 import cv2
 import numpy as np
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
 
-
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-
-video_capture = cv2.VideoCapture(1)
+face_classifier=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+classifier = load_model('EmotionDetectionModel.h5')
+class_labels=['Angry','Happy','Neutral','Sad','Surprise']
+cap=cv2.VideoCapture(1)
 
 while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+    ret,frame=cap.read()
+    labels=[]
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=face_classifier.detectMultiScale(gray,1.3,5)
 
-    # flips video vertically
-    frame = cv2.flip(frame,1)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray=gray[y:y+h,x:x+w]
+        roi_gray=cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
 
-    faces = faceCascade.detectMultiScale(
-        frame,
-        scaleFactor=1.3,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
+        if np.sum([roi_gray])!=0:
+            roi=roi_gray.astype('float')/255.0
+            roi=img_to_array(roi)
+            roi=np.expand_dims(roi,axis=0)
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            preds=classifier.predict(roi)[0]
+            label=class_labels[preds.argmax()]
+            label_position=(x,y)
+            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+        else:
+            cv2.putText(frame,'No Face Found',(20,20),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-
+    cv2.imshow('Emotion Detector',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-# When everything is done, release the capture
-video_capture.release()
+cap.release()
 cv2.destroyAllWindows()
